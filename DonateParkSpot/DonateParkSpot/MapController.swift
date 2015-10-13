9ade4d00a2b9cc783b6e8a16ae24c6eac9ed4483
xyuan14 <yuanxiaoyu01@gmail.com>
2015-10-12 20:04:34 -0500

    segue example

---
+++ DonateParkSpot/DonateParkSpot/MapController.swift
@@ -0,0 +1,14 @@
+//
+//  MapController.swift
+//  DonateParkSpot
+//
+//  Created by Xiaoyu Yuan on 10/12/15.
+//  Copyright © 2015 SE491. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class MapController : UIViewController {
+    
+}
