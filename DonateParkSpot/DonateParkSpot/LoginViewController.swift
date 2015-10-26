2c3d4744aa4a137c994916912cdf15cbf34fef58
Apple <apple@appledemacbook-pro.local>
2015-10-26 16:50:07 -0500

    missing files

---
+++ DonateParkSpot/DonateParkSpot/LoginViewController.swift
@@ -0,0 +1,89 @@
+//
+//  LoginViewController.swift
+//  DonateParkSpot
+//
+//  Created by Apple on 10/23/15.
+//  Copyright © 2015 Apple. All rights reserved.
+//
+
+import UIKit
+import Parse
+
+class LoginViewController: UIViewController {
+    
+    
+    @IBOutlet weak var userEmailTextField: UITextField!
+    
+    @IBOutlet weak var userPasswordTextField: UITextField!
+    override func viewDidLoad() {
+        
+        super.viewDidLoad()
+    }
+    
+    
+    override func didReceiveMemoryWarning() {
+        
+        
+        super.didReceiveMemoryWarning()
+    }
+
+
+    @IBAction func LoginButtonTapped(sender: AnyObject) {
+        
+        
+        
+     Login()
+    }
+    
+    
+    func Login()
+    {
+        var user = PFUser()
+        user.username = userEmailTextField.text!
+        user.password = userPasswordTextField.text!
+        PFUser.logInWithUsernameInBackground(userEmailTextField.text!, password: userPasswordTextField.text!, block:  {
+            (User : PFUser?, Error : NSError?) -> Void in
+        
+            
+            if Error == nil{
+            
+                
+                NSUserDefaults.standardUserDefaults().setBool(true, forKey: "isUserLoggedIn")
+                
+                NSUserDefaults.standardUserDefaults().synchronize();
+                self.dismissViewControllerAnimated(true, completion: nil);
+
+            }
+            
+            
+            else
+            {
+                
+                
+                NSUserDefaults.standardUserDefaults().setBool(false, forKey: "isUserLoggedIn")
+                
+                NSUserDefaults.standardUserDefaults().synchronize();
+                
+                self.displayMyAlertMessage("Email or password is not valid");
+                
+                self.dismissViewControllerAnimated(true, completion: nil);
+
+                
+            }
+        })
+    
+    }
+    
+    func displayMyAlertMessage(usermessage:String)
+        
+    {
+        let myAlert=UIAlertController(title: "Alert", message:usermessage,
+            preferredStyle: UIAlertControllerStyle.Alert);
+        let okayAction=UIAlertAction(title: "Okay", style:UIAlertActionStyle.Default, handler:nil)
+        myAlert.addAction(okayAction);
+        self.presentViewController(myAlert, animated: true, completion: nil);
+        
+        
+    }
+
+}
