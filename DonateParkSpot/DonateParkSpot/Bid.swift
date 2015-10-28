6b3c7520cd4ac220a1444a82823bf6fc8394f8d6
Rafael Guerra <faelwar@gmail.com>
2015-10-27 21:25:22 -0500

    bid model

---
+++ DonateParkSpot/DonateParkSpot/Bid.swift
@@ -0,0 +1,15 @@
+//
+//  Bid.swift
+//  DonateParkSpot
+//
+//  Created by Rafael Guerra on 10/27/15.
+//  Copyright © 2015 Apple. All rights reserved.
+//
+
+import Foundation
+
+class Bid {
+    var spot : Spot?
+    var timestamp : NSDate?
+    var value : Double?
+}
