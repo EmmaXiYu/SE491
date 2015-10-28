646cdd6d2a3b87ef9399ed5d97aee11196555555
Rafael Guerra <faelwar@gmail.com>
2015-10-28 18:38:54 -0500

    Search history Model

---
+++ DonateParkSpot/DonateParkSpot/SearchHistory.swift
@@ -0,0 +1,15 @@
+//
+//  SearchHistory.swift
+//  DonateParkSpot
+//
+//  Created by Rafael Guerra on 10/28/15.
+//  Copyright © 2015 Apple. All rights reserved.
+//
+
+import Foundation
+
+class SearchHistory {
+    var location : Location?
+    var timestamp : NSDate?
+    var address : String?
+}
