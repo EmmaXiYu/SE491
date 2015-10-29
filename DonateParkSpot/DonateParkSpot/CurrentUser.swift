c903d3bd642a99cb10394829cafb293a8f9d1751
Rafael Guerra <faelwar@gmail.com>
2015-10-28 20:41:14 -0500

    CurrentUser

---
+++ DonateParkSpot/DonateParkSpot/CurrentUser.swift
@@ -0,0 +1,13 @@
+//
+//  CurrentUser.swift
+//  DonateParkSpot
+//
+//  Created by Rafael Guerra on 10/28/15.
+//  Copyright © 2015 Apple. All rights reserved.
+//
+
+import Foundation
+
+class CurrentUser {
+    static var currentUser : User?
+}
