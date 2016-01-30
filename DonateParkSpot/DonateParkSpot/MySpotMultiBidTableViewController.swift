5c3c9bbb748482e2e557a7978ebfc862e38a8484
xyuan14 <yuanxiaoyu01@gmail.com>
2016-01-29 18:53:45 -0600

    Rating system

--- DonateParkSpot/DonateParkSpot/MySpotMultiBidTableViewController.swift
+++ DonateParkSpot/DonateParkSpot/MySpotMultiBidTableViewController.swift
@@ -13,6 +13,9 @@ import Parse
  var DetailSpot : Spot = Spot()
     var rating: Double = 0;
     var count: Int = 0;
+    var ratingScore = [String:Double]()
+    var ratingCount = [String:Int]()
+    
 var bidNoPayAutoCancelTime : Int = 4  // Set a intitial value,
  //var currentIndex : Int =  -1
     override func viewDidLoad() {
@@ -36,6 +39,35 @@ var bidNoPayAutoCancelTime : Int = 4  // Set a intitial value,
             self.tableView.reloadData()
         }
     }
+    
+    
+    func getRating(){
+        var query: PFQuery = PFQuery()
+        query = PFQuery(className: "Rating")
+        query.findObjectsInBackgroundWithBlock{(objects:[PFObject]?,error:NSError?) -> Void in
+            if error == nil{
+                for object in objects!{
+                    let name = object["userName"] as! String
+                    let score = object["score"] as! Int
+                    if(self.ratingScore[name] != nil){
+                        self.ratingScore[name] = self.ratingScore[name]!+Double(score)
+                        self.ratingCount[name] = self.ratingCount[name]!+1
+                    }
+                    else{
+                        self.ratingScore[name] = Double(score)
+                        self.ratingCount[name] = 0
+                    }
+                }
+                for name in self.ratingScore.keys{
+                    self.ratingScore[name] = self.formulateScore(self.ratingScore[name]!,count: self.ratingCount[name]!)
+                }
+                
+            }
+        }
+    }
+    
+
+    
     public func GetBidList(spotid: String)  {
         self.getBidNoPayAutoCancelTime()
         
@@ -451,7 +483,21 @@ MySpotMultiBidTableViewCell
         return (score/Double(count)+1.0)*2.5;
     }
     
+    func formulateScore(rating:Double,count:Int) ->Double{
+        return (rating/Double(count)+1.0)*2.5;
+    }
     
+    func updateRating(username:String, score: Int,statusId:Int)->Void{
+        let update = PFObject(className: "Rating")
+        update["name"] = username
+        update["socre"] = score
+        update["statusId"] = statusId
+        update.saveInBackgroundWithBlock{
+            (success:Bool,error:NSError?) -> Void in
+            if(success){
+            }
+        }
+    }
     
     /*
     @IBAction func AcceptButton_Clicked(sender: UIButton) {
