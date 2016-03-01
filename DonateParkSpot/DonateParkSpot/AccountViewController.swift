b324173b2e6d19b00c4d88e486c31b13513be903
Rafael Guerra <faelwar@gmail.com>
2016-02-29 19:37:56 -0600

    Image fix

--- DonateParkSpot/DonateParkSpot/AccountViewController.swift
+++ DonateParkSpot/DonateParkSpot/AccountViewController.swift
@@ -77,7 +77,8 @@ class AccountViewController: UIViewController , UIImagePickerControllerDelegate,
         self.getRating()
                 Menu.target = self.revealViewController()
         Menu.action = Selector("revealToggle:")
-        
+        self.ImageField.image = UIImage(named: "default-user")
+
         self.view.addGestureRecognizer(self.revealViewController().panGestureRecognizer())
         accountName.text = PFUser.currentUser()!["username"] as? String
         cellPhoneNumber.text = PFUser.currentUser()! ["PhoneNumber"] as? String
